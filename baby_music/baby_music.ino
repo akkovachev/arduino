2a98df43cf42bc612b77627223cf292889b187ef
Atanas Kovachev <kovachev90@gmail.com>
2019-03-08 19:20:15 +0000

    Arduino Repo First Project

---
+++ baby_music/baby_music.ino
@@ -0,0 +1,63 @@
+/*
+  Melody
+
+  Plays a melody
+
+  circuit:
+  - 8 ohm speaker on digital pin 8
+
+  created 21 Jan 2010
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Tone
+*/
+
+#include "pitches.h"
+
+// notes in the melody:
+int Pirates_note[] = {
+  NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,
+NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, 
+NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_D4,  
+NOTE_A3, NOTE_C4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_F4, 
+NOTE_F4, NOTE_G4, NOTE_E4, NOTE_E4, NOTE_D4, NOTE_C4, NOTE_C4, NOTE_D4,
+0, NOTE_A3, NOTE_C4, NOTE_B3, NOTE_D4, NOTE_B3, NOTE_E4, NOTE_F4,
+NOTE_F4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_C4, NOTE_D4, NOTE_C4,
+NOTE_D4, 0, 0, NOTE_A3, NOTE_C4, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_F4,
+NOTE_G4, NOTE_G4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_A4, NOTE_G4,
+NOTE_A4, NOTE_D4, 0, NOTE_D4, NOTE_E3, NOTE_F4, NOTE_F4, NOTE_G4, NOTE_A4, 
+NOTE_D4, 0, NOTE_D4, NOTE_F4, NOTE_E4, NOTE_E4, NOTE_F4, NOTE_D4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int Pirates_duration[] = {
+  4,8,4,8,4,8,8,8,8,4,8,4,8,4,8,8,8,8,4,8,4,8,
+4,8,8,8,8,4,4,8,8,4,4,8,8,4,4,8,8,
+8,4,8,8,8,4,4,8,8,4,4,8,8,4,4,8,4,
+4,8,8,8,8,4,4,8,8,4,4,8,8,4,4,8,8,
+8,4,8,8,8,4,4,4,8,4,8,8,8,4,4,8,8
+};
+
+void Play_Pirates()
+{
+  for (int thisNote = 0; thisNote < (sizeof(Pirates_note)/sizeof(int)); thisNote++) {
+    int noteDuration = 1000 / Pirates_duration[thisNote];//convert duration to time delay
+    tone(8, Pirates_note[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.05; //Here 1.05 is tempo, increase to play it slower
+    delay(pauseBetweenNotes);
+    noTone(8);
+    }
+}
+void setup() {
+  // iterate over the notes of the melody:
+  Play_Pirates();
+}
+
+void loop() {
+//   Play_Pirates();
+//   delay(1000);
+  // no need to repeat the melody.
+}
